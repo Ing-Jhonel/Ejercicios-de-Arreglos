006ba8061a6621977314995c4577fc41fb1bddf9
Jhonel <jgapazap@unjbg.edu.pe>
2024-06-29 12:36:00 -0500

    4.2 modificacion: mejore interfaz

--- 4ordenalfebetico.cpp
+++ 4ordenalfebetico.cpp
@@ -9,22 +9,30 @@ int main() {
     const int max_nombres=100; // Define el tamaño máximo del arreglo nombres. Máximo número de nombres a leer
     string nombres[max_nombres]; // Arreglo para almacenar los nombres (nombre[100])
     int num_nombres; //Variable que almacenará cuántos nombres se ingresarán.
-
+	
+	cout << "ORDENA NOMBRES ALFABETICAMENTE" << endl;
+	cout << "******************************" << endl << endl;
 	//2. PEDIR CANTIDAD DE NOMBRES A INGRESAR
     // Pedir al usuario que ingrese el número de nombres a ingresar
-    cout << "Ingrese el numero de nombres a ingresar: ";
+    cout << "*Ingrese el numero de nombres a ingresar: ";
     cin >> num_nombres;
 
+	cout << endl;
+	
 	//3. VERIFICAR CANTIDAD DE NOMBRES VALIDA
     // Verificar que el número de nombres sea válido
     while (num_nombres<=0 || num_nombres>max_nombres) { // no n <=0  o   n > 100
         cout << "Invalido. Intente de nuevo: ";
         cin >> num_nombres;
+        if(num_nombres>0 && num_nombres<=max_nombres){ // para dar espacio si el bucle termina
+        	cout << endl;
+		}
     }
 	
 	//4. LEER NOMBRES 
     // Pedir al usuario que ingrese los nombres
-    cout << "Ingrese los nombres uno por uno:" << endl;
+    cout << "*Ingrese los nombres uno por uno:" << endl;
+    cout << endl;
     for (int i=0; i<num_nombres; i++) {
         cout << "Nombre " << i+1 << ": ";
         cin >> nombres[i];
@@ -40,6 +48,7 @@ int main() {
     //6. MOSTRAR NOMBRES ORDENADOS
     // al estar ya ordenados, solo faltaria imprimirlos
     cout << "\nNombres ordenados alfabeticamente:" << endl;
+    cout << endl;
     for (int i=0; i<num_nombres; i++) {
         cout << nombres[i] << endl;
     }
