8263e80e750126639b88488fd758dd872aa15d2d
Jhonel <jgapazap@unjbg.edu.pe>
2024-06-29 12:29:39 -0500

    4.1 modificacion: especifique funcion de cada codigo

--- 4ordenalfebetico.cpp
+++ 4ordenalfebetico.cpp
@@ -1,31 +1,44 @@
 #include <iostream>
 #include <string>
-#include <algorithm>
+#include <algorithm> // Para std::sort
 using namespace std;
 
 int main() {
 	
+	//1. DECLARAR VARIABLES
+    const int max_nombres=100; // Define el tamaño máximo del arreglo nombres. Máximo número de nombres a leer
+    string nombres[max_nombres]; // Arreglo para almacenar los nombres (nombre[100])
+    int num_nombres; //Variable que almacenará cuántos nombres se ingresarán.
 
-    const int max_nombres=100;
-    string nombres[max_nombres]; 
-    int num_nombres; 
-
+	//2. PEDIR CANTIDAD DE NOMBRES A INGRESAR
+    // Pedir al usuario que ingrese el número de nombres a ingresar
     cout << "Ingrese el numero de nombres a ingresar: ";
     cin >> num_nombres;
 
-    while (num_nombres<=0 || num_nombres>max_nombres) { 
+	//3. VERIFICAR CANTIDAD DE NOMBRES VALIDA
+    // Verificar que el número de nombres sea válido
+    while (num_nombres<=0 || num_nombres>max_nombres) { // no n <=0  o   n > 100
         cout << "Invalido. Intente de nuevo: ";
         cin >> num_nombres;
     }
-
+	
+	//4. LEER NOMBRES 
+    // Pedir al usuario que ingrese los nombres
     cout << "Ingrese los nombres uno por uno:" << endl;
     for (int i=0; i<num_nombres; i++) {
         cout << "Nombre " << i+1 << ": ";
         cin >> nombres[i];
     }
-
+	
+	//5. ORDENAR LOS NOMBRES ALFABETICAMENTE
+	//nombres= es el primer arreglo
+	// nombres + num_nomnbres = limite 
+	// a <= x <= b
+	// sort lo que hace es ordenar el arreglo nombres, alfabeticamente
     sort(nombres, nombres + num_nombres);
 
+    //6. MOSTRAR NOMBRES ORDENADOS
+    // al estar ya ordenados, solo faltaria imprimirlos
     cout << "\nNombres ordenados alfabeticamente:" << endl;
     for (int i=0; i<num_nombres; i++) {
         cout << nombres[i] << endl;
