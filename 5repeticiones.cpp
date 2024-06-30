4ccc9f0f58afff658256d29f587c3e74ce6290bb
Jhonel <jgapazap@unjbg.edu.pe>
2024-06-30 11:35:51 -0500

    5.1 modificacacion: especifique funcion de cada codigo

--- 5repeticiones.cpp
+++ 5repeticiones.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main() {
     int n;
     
+    //1. LEER NUMEROS
     cout << "Ingrese la cantidad de numeros: ";
     cin >> n;
 
@@ -18,27 +19,32 @@ int main() {
         cin >> numeros[i];
     }
 
+	// 2. DECLARAR VARIABLES
+    // Inicialización de variables para el mayor y el menor
     int mayor = numeros[0];
-    int menor = numeros[0];
+    int menor = numeros[0];// a ambos le ponemos numeros[0] siendo el primer elemento del arreglo, porque primeramente
+    // este sera el primer mayor y el primer menor, por eso se inicializa con este.
     int contadorMayor = 1;
     int contadorMenor = 1;
 
-    for (int i=1; i<n; i++){
+    //3. BUSCAR MAYOR Y MENOR
+    for (int i=1; i<n; i++){ // se cuenta desde el 1, porque el mayor ya esta en el arreglo 0
     	if(numeros[i] > mayor){ 
-    		mayor = numeros[i];
-    		contadorMayor=1;
+    		mayor = numeros[i]; // cambian por cada iteracion que se cumpla
+    		contadorMayor=1; // esto es para reiniciar el contador cada vez que se halla un nuevo mayor
 		} else if (numeros[i] == mayor){
-			contadorMayor++;
+			contadorMayor++; // si se cumple, se agrega +1
 		}
 		
 		if(numeros[i] < menor){
-			menor = numeros[i];
-			contadorMenor=1;
+			menor = numeros[i]; // cambian por cada iteracion que se cumpla
+			contadorMenor=1;// esto es para reiniciar el contador cada vez que se halla un nuevo menor
 		} else if (numeros[i] == menor){
-			contadorMenor++;
+			contadorMenor++;  // si se cumple, se agrega +1
 		}
 	}
 
+    //4. MOSTRAR RESULTADOS
     cout << "El mayor numero ingresado es: " << mayor << endl;
     cout << "Se repite " << contadorMayor << " veces." << endl;
     cout << "El menor numero ingresado es: " << menor << endl;
