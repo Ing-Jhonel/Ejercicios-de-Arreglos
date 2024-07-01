3da8b14a2abe04c5043d10f4019c969e587318b4
Jhonel <jgapazap@unjbg.edu.pe>
2024-07-01 12:47:20 -0500

    10.2 modificacion: mejore interfaz

--- 10sumapositivosnegativos.cpp
+++ 10sumapositivosnegativos.cpp
@@ -5,10 +5,13 @@ int main(){
 	
 	//1. LEER CANTIDAD DE ELEMENTOS A INGRESAR
 	int n;
+	cout << "SUMA LOS POSITIVOS Y NEGATIVOS DE TUS ELEMENTOS" << endl;
+	cout << "***********************************************" << endl << endl;
 	cout << "Indique la cantidad de elementos que quiere ingresar: ";
 	cin >> n;
 	int arreglo[n];
 	
+	cout << endl;
 	//2. PEDIR CADA ELEMENTO Y ALMACENARLOS EN UN ARREGLO
 	int sumanegativos=0, sumapositivos=0;
 	for(int i=0; i<n; i++){
@@ -21,8 +24,12 @@ int main(){
 			sumanegativos+=arreglo[i];
 		}
 	}
+	
+	cout << endl;
 	//4. IMPRIMIR 
 	cout << "Suma de los elementos positivos: " << sumapositivos << endl;
 	cout << "Suma de los elementos negativos: " << sumanegativos << endl;
 	return 0;
 }
+
+// SE PODRIA REEMPLAZAR EL ARREGLO[i] POR UN UNA VARIABLE "num", la diFERENCIA ES DESPUES NO PODREMOS USAR CADA ELEMENTO INGRESADO
