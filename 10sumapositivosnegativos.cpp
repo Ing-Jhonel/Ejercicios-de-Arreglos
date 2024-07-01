ec0377a6bbe1f0655447f0d52ba01ca397c51b05
Jhonel <jgapazap@unjbg.edu.pe>
2024-07-01 12:40:46 -0500

    10.1 modificacion: especifique partes del codigo

--- 10sumapositivosnegativos.cpp
+++ 10sumapositivosnegativos.cpp
@@ -3,21 +3,25 @@ using namespace std;
 
 int main(){
 	
+	//1. LEER CANTIDAD DE ELEMENTOS A INGRESAR
 	int n;
 	cout << "Indique la cantidad de elementos que quiere ingresar: ";
 	cin >> n;
 	int arreglo[n];
 	
+	//2. PEDIR CADA ELEMENTO Y ALMACENARLOS EN UN ARREGLO
 	int sumanegativos=0, sumapositivos=0;
 	for(int i=0; i<n; i++){
 		cout << "Elemento " << i+1 << ": ";
 		cin >> arreglo[i];
+		// 3. SUMAR LOS NEGATIVOS Y POSITIVOS
 		if(arreglo[i]>0){
 			sumapositivos+=arreglo[i];
 		} else{
 			sumanegativos+=arreglo[i];
 		}
 	}
+	//4. IMPRIMIR 
 	cout << "Suma de los elementos positivos: " << sumapositivos << endl;
 	cout << "Suma de los elementos negativos: " << sumanegativos << endl;
 	return 0;
