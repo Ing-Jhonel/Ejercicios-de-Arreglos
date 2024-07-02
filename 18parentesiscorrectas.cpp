5c8a3e5f63f7438d452886a91a9ad948a673a0c1
Jhonel <jgapazap@unjbg.edu.pe>
2024-07-02 10:45:50 -0500

    Agregue 18parentesiscorrectos

---
+++ 18parentesiscorrectas.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+bool verificarBalanceoParentesis(const string& expresion) {
+    stack<char> pila;
+
+    for (size_t i = 0; i < expresion.length(); ++i) {
+        char caracter = expresion[i];
+        if (caracter == '(') {
+            pila.push(caracter);
+        } else if (caracter == ')') {
+            if (pila.empty() || pila.top() != '(') {
+                return false; // Hay un cierre sin un paréntesis de apertura correspondiente
+            }
+            pila.pop();
+        }
+    }
+
+    return pila.empty(); // La pila debe estar vacía para que estén balanceados todos los paréntesis
+}
+
+int main() {
+    string expresion;
+
+    // Pedir al usuario que introduzca la expresión
+    cout << "Introduce una expresion con parentesis para verificar su balanceo: ";
+    getline(cin, expresion); // Leer toda la línea introducida por el usuario
+
+    // Verificar el balanceo de paréntesis
+    if (verificarBalanceoParentesis(expresion)) {
+        cout << "La expresion esta balanceada." << endl;
+    } else {
+        cout << "La expresion no esta balanceada." << endl;
+    }
+
+    return 0;
+}
+
+
+
+
+
