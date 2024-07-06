57e3607d91cc50f86ae539728b74691ceae8ac2c
Jhonel <jgapazap@unjbg.edu.pe>
2024-07-06 12:32:57 -0500

    Avance 4

---
+++ 4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	string nombres;
+	cin >> nombres;
+	
+	char abecedario[]="abcdefghijklmnopqrstuvwxyz";
+	for(int i=0; i<nombres.length(); i++){
+		if(nombres[i]==' '){
+			for()
+		}
+	}
+	return 0;
+}
